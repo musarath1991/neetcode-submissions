6f6507e2cf5ddebd4c2978ea8c4bac654ece4f7d
musarath1991 <112262106+musarath1991@users.noreply.github.com>
2026-04-28 14:41:12 +0530

    Add: foreign-dictionary - submission-14

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-14.py
@@ -0,0 +1,33 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        adj = defaultdict(set)
+        indegree = {c:0 for word in words for c in word}
+        for i in range(len(words)-1):
+            w1 = words[i]
+            w2 = words[i+1]
+            if len(w1) > len(w2) and w1.startswith(w2):
+                return ""
+            for j in range(min(len(w1), len(w2))):
+                if w1[j] != w2[j]:
+                    if w2[j] not in adj[w1[j]]:
+                        adj[w1[j]].add(w2[j])
+                        indegree[w2[j]] += 1
+                    break
+        queue = deque()
+        for c in indegree:
+            if indegree[c] == 0:
+                queue.append(c)
+        res = []
+        while queue:
+            ch = queue.popleft()
+            res.append(ch)
+            for neigh in adj[ch]:
+                indegree[neigh] -= 1
+                if indegree[neigh] == 0:
+                    queue.append(neigh)
+        
+        if len(res) != len(indegree):
+            return ""
+        
+        return "".join(res)
+
