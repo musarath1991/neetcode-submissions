b5827afb9a48475f5a20be39e2201c7249cd16e7
musarath1991 <112262106+musarath1991@users.noreply.github.com>
2026-05-24 15:38:38 +0530

    Add: evaluate-division - submission-4

---
+++ Data Structures & Algorithms/evaluate-division/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        adj = defaultdict(list)
+        for i, eq in enumerate(equations):
+            a, b = eq
+            adj[a].append((b, values[i]))
+            adj[b].append((a, 1/values[i]))
+        def bfs(source, target):
+            if source not in adj or target not in adj:
+                return -1
+            queue = deque()
+            queue.append((source, 1))
+            visited = set()
+            visited.add(source)
+            while queue:
+                node, w = queue.popleft()
+                if node == target:
+                    return w
+                for neigh, weight in adj[node]:
+                    if neigh not in visited:
+                        visited.add(neigh)
+                        queue.append((neigh, w*weight))
+            return -1 
+
+        res = []
+        for u, v in queries:
+            res.append(bfs(u, v))
+        return res
